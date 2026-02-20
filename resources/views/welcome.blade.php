b5347a095df16322499e43e3ccfdffe01227ab44
syawalqi <galihmaulanasyawalqi@gmail.com>
2026-02-20 11:02:20 +0700

    feat: add default Laravel welcome blade template with styling and script includes.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,7 +112,7 @@
                     </ul>
                     <ul class="flex gap-3 text-sm leading-normal">
                         <li>
-                            <a href="https://cloud.laravel.com" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                            <a href="https://github.com/syawalqi/Pertemuan1-PWF-C-20230140248.git" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
                                 View Github
                             </a>
                         </li>
